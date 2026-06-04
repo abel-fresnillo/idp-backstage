0da5eb5fb12d14c62f8625edf03d7e4fc05304ae
Abel Fresnillo <abel@Abels-MacBook-Air.local>
2026-06-03 21:02:21 -0400

    Fixes Render deployment

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,28 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import { SignInPage } from '@backstage/core-components';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
 import { navModule } from './modules/nav';
 
+const githubSignInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props => (
+      <SignInPage
+        {...props}
+        providers={[
+          {
+            id: 'github-auth-provider',
+            title: 'GitHub',
+            message: 'Sign in with GitHub',
+            apiRef: githubAuthApiRef,
+          },
+        ]}
+      />
+    ),
+  },
+});
+
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [catalogPlugin, navModule, githubSignInPage],
 });
